f3f1d3705c33b5ada4de93af211f3448e797aa7d
papi721-main <99392252+papi721-main@users.noreply.github.com>
2025-06-12 12:58:08 +0300

    feat: finish task 1

---
+++ programming_paradigm/robust_division_calculator.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+
+def safe_divide(numerator, denominator):
+    try:
+        result = float(numerator) / float(denominator)
+        return f"The result of the division is {result:.1f}"
+    except ZeroDivisionError:
+        return "Error: Cannot divide by zero."
+    except ValueError:
+        return "Error: Please enter numeric values only."
